2e58b80ffaebc29458fd3d790720c4b6f76d00e1
Tania Manco <belalov.tania16@gmail.com>
2020-01-06 18:39:14 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import CatComponent from './CatComponent.js';
+import EinsteinQuoteComponent from './EinsteinQuoteComponent.js';
+import MouseComponent from './MouseComponent.js';
 
 class App extends Component {
 	render() {
